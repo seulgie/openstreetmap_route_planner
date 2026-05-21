9c013210ec3767516b46a42be190a9d47f710395
seulgie <gkstmfrl917@naver.com>
2026-05-21 15:06:01 +0200

    modified src/route_planner.cpp and src/route_planner.h

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -1,5 +1,7 @@
 #include "route_planner.h"
 #include <algorithm>
+#include <fstream>
+#include <iomanip>
 
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
     // Convert inputs to percentage:
@@ -8,30 +10,20 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     end_x *= 0.01;
     end_y *= 0.01;
 
-    // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
+    // Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
     start_node = &m_Model.FindClosestNode(start_x, start_y);
     end_node = &m_Model.FindClosestNode(end_x, end_y);
 }
 
 
-// TODO 3: Implement the CalculateHValue method.
-// Tips:
-// - You can use the distance to the end_node for the h value.
-// - Node objects have a distance method to determine the distance to another node.
-
+// Implement the CalculateHValue method.
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
     return node->distance(*end_node);
 }
 
 
-// TODO 4: Complete the AddNeighbors method to expand the current node by adding all unvisited neighbors to the open list.
-// Tips:
-// - Use the FindNeighbors() method of the current_node to populate current_node.neighbors vector with all the neighbors.
-// - For each node in current_node.neighbors, set the parent, the h_value, the g_value. 
-// - Use CalculateHValue below to implement the h-Value calculation.
-// - For each node in current_node.neighbors, add the neighbor to open_list and set the node's visited attribute to true.
-
+// AddNeighbors method to expand the current node by adding all unvisited neighbors to the open list.
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
     current_node->FindNeighbors();
 
@@ -48,13 +40,7 @@ void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
 }
 
 
-// TODO 5: Complete the NextNode method to sort the open list and return the next node.
-// Tips:
-// - Sort the open_list according to the sum of the h value and g value.
-// - Create a pointer to the node in the list with the lowest sum.
-// - Remove that node from the open_list.
-// - Return the pointer.
-
+// NextNode method to sort the open list and return the next node.
 RouteModel::Node *RoutePlanner::NextNode() {
     std::sort(open_list.begin(), open_list.end(),
                 [](const auto *a, const auto *b){
@@ -66,20 +52,12 @@ RouteModel::Node *RoutePlanner::NextNode() {
 }
 
 
-// TODO 6: Complete the ConstructFinalPath method to return the final path found from your A* search.
-// Tips:
-// - This method should take the current (final) node as an argument and iteratively follow the 
-//   chain of parents of nodes until the starting node is found.
-// - For each node in the chain, add the distance from the node to its parent to the distance variable.
-// - The returned vector should be in the correct order: the start node should be the first element
-//   of the vector, the end node should be the last element.
-
+// ConstructFinalPath method to return the final path found from your A* search.
 std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node) {
     // Create path_found vector
     distance = 0.0f;
     std::vector<RouteModel::Node> path_found;
 
-    // TODO: Implement your solution here.
     while (current_node->parent != nullptr){
         path_found.push_back(*current_node);
         distance += current_node->distance(*current_node->parent);
@@ -95,18 +73,58 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
 
 }
 
+// ExportGeoJSON method to export the final path as a GeoJSON file.
+void RoutePlanner::ExportGeoJSON(const std::vector<RouteModel::Node>& path) {
+    std::ofstream file("route.geojson");
 
-// TODO 7: Write the A* Search algorithm here.
-// Tips:
-// - Use the AddNeighbors method to add all of the neighbors of the current node to the open_list.
-// - Use the NextNode() method to sort the open_list and return the next node.
-// - When the search has reached the end_node, use the ConstructFinalPath method to return the final path that was found.
-// - Store the final path in the m_Model.path attribute before the method exits. This path will then be displayed on the map tile.
+    if (!file.is_open()) {
+        std::cerr << "Failed to create GeoJSON file.\n";
+        return;
+    }
 
+    file << std::fixed << std::setprecision(6);
+
+    file << "{\n";
+    file << R"(  "type": "FeatureCollection",)" << "\n";
+    file << R"(  "features": [)" << "\n";
+    file << "    {\n";
+    file << R"(      "type": "Feature",)" << "\n";
+    file << R"(      "geometry": {)" << "\n";
+    file << R"(        "type": "LineString",)" << "\n";
+    file << R"(        "coordinates": [)" << "\n";
+
+    for (size_t i = 0; i < path.size(); ++i) {
+        file << "          ["
+             << path[i].x << ", "
+             << path[i].y << "]";
+
+        if (i < path.size() - 1)
+            file << ",";
+
+        file << "\n";
+    }
+
+    file << "        ]\n";
+    file << "      },\n";
+
+    file << R"(      "properties": {)" << "\n";
+    file << R"(        "distance_meters": )" << distance << "\n";
+    file << "      }\n";
+
+    file << "    }\n";
+    file << "  ]\n";
+    file << "}\n";
+
+    file.close();
+
+    std::cout << "GeoJSON exported: route.geojson\n";
+}
+
+
+// A* Search algorithm here.
 void RoutePlanner::AStarSearch() {
     RouteModel::Node *current_node = nullptr;
 
-    // TODO: Implement your solution here.
     start_node->visited = true;
     open_list.push_back(start_node);
 
@@ -115,6 +133,7 @@ void RoutePlanner::AStarSearch() {
 
         if (current_node == end_node) {
             m_Model.path = ConstructFinalPath(current_node);
+            ExportGeoJSON(m_Model.path);
             return;
         }
 
--- src/route_planner.h
+++ src/route_planner.h
@@ -20,6 +20,8 @@ class RoutePlanner {
     std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
     RouteModel::Node *NextNode();
 
+    void ExportGeoJSON(const std::vector<RouteModel::Node>& path);
+
   private:
     // Add private variables or methods declarations here.
     std::vector<RouteModel::Node*> open_list;
